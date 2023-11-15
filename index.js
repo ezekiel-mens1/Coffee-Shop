bc98039a5418b8982319d7122dd6dc7d468ed60e
ezekiel-mens1 <ezekielmensah517@gmail.com>
2023-11-15 17:32:26 +0000

    navbar fixed

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const header = document.querySelector('header');
 
 window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky", window.scroll > 60)
+    header.classList.toggle("sticky", window.scrollY > 60)
 });
 
 let menu= document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let navbar = document.getElementsByClassName('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+
+// const menulist =document.getElementsByClassName('navbar');
+// menulist.style.maxHeight ="0px";
+
+// function togglemenu(){
+//     if(menulist.style.maxHeight =="0px")
+//     {
+//         menulist.style.maxHeight = "130px";
+//     }
+//     else{
+//         menulist.style.maxHeight = "0px";
+//     }
+// }
