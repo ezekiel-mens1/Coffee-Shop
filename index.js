98d2308f7b0f4c269653c789c7067538af4e0622
ezekiel-mens1 <ezekielmensah517@gmail.com>
2023-11-22 23:57:24 +0000

    still fixing navbar

--- index.js
+++ index.js
@@ -13,15 +13,3 @@ menu.onclick = () => {
 }
 
 
-// const menulist =document.getElementsByClassName('navbar');
-// menulist.style.maxHeight ="0px";
-
-// function togglemenu(){
-//     if(menulist.style.maxHeight =="0px")
-//     {
-//         menulist.style.maxHeight = "130px";
-//     }
-//     else{
-//         menulist.style.maxHeight = "0px";
-//     }
-// }
